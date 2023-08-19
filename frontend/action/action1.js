834f8f7567e0fb4205cd32f1b9a8716a8970bc00
YousefAwwad497 <138987138+YousefAwwad497@users.noreply.github.com>
2023-08-19 16:32:36 +0300

    Add files via upload

---
+++ frontend/action/action1.js
@@ -0,0 +1,21 @@
+//var num1 = [5,6,7];
+//
+//for (var i = 0; i < num1.length; i++) {
+//    if (num1[i]>10) {
+//    return num1[i] 
+//  }
+//  }
+//
+////__________________________________
+//
+//
+//var x = [1,2,3];
+//for(var x % 2 ){
+//    if (x % 2 ==0 )
+//    return x;
+//}
+//
+//
+//var y = ["football","volleyball","basketball"]
+//for(var y )
+//
